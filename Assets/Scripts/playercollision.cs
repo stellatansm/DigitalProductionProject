6f356ed294b45d03a92bbc839f23208245200004
uhveahn <thearia27@gmail.com>
2023-01-21 15:17:31 +0800

    *crying*

--- Assets/Scripts/playercollision.cs
+++ Assets/Scripts/playercollision.cs
@@ -6,7 +6,7 @@ public class playercollision : MonoBehaviour
     {
         if (collision.transform.tag == "ghost")
         {
-            Debug.Log("fuck you");
+            PlayerManager.isGameOver = true;
         }
     }
 } 
